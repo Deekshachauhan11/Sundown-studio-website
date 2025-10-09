6e5f53f59f464631f0ea947ee963e03a3fdb8c5f
Deekshachauhan11 <cdeeksha0011@gmail.com>
2025-10-09 14:12:03 +0530

    conplete website

--- script.js
+++ script.js
@@ -2,3 +2,37 @@
 //     el: document.querySelector('main'),
 //     smooth: true
 // });
+
+let elem = document.querySelector(".element");
+let fixed = document.querySelector(".fixed-img")
+elem.addEventListener("mouseenter", function(){
+    fixed.style.display = "block";
+});
+
+elem.addEventListener("mouseleave", function(){
+    fixed.style.display = "none";
+});
+
+let elems = document.querySelectorAll(".elem");
+elems.forEach(function(dets){
+   dets.addEventListener("mouseenter", function(){
+    let img = dets.getAttribute("data-img")
+    fixed.style.backgroundImage = `url(${img})`
+   })
+})
+
+function swiper(){
+  var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 50,
+    });
+}
+swiper();
+
+
+let load = document.querySelector(".loader");
+setTimeout(function(){
+   load.style.top = "-100%"
+}, 4000);
+
